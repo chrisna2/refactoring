0488961d1969b5cae0fa1b6c9ee1901690eb4f97
kosta <kosta@DESKTOP-H317250.mshome.net>
2019-08-24 15:59:40 +0900

    메서드 정리 1: 긴 로직은 메서드로 분리

--- src/code/refactoring/methodarrange_1/Owe.java
+++ src/code/refactoring/methodarrange_1/Owe.java
@@ -59,5 +59,28 @@ public class Owe {
 	}
 
 	
+	/* 
+	//as-is
+	public void printOwing(double previousAmount) {
+
+	    Enumeration e = _orders.elements();
+	    double outstanding = previousAmount*1.2;
+	    System.out.println("************************");
+	    System.out.println("*******고객 외상*******");
+	    System.out.println("************************");
+
+	    // 외상액 계산
+	    while(e.hasMoreElements()) {
+	        Order each = (Order)e.nextElement();
+	        outstanding += each.getAmount();
+	    }
+
+	    // 세부내역 출력
+	    System.out.println("name: " + _name);
+	    System.out.println("amount: " + outstanding);
+
+	}
+	 */
+	
 	
 }
