b28ac3342a209cec3aecbc3ff1db33a6951bde5c
kosta <kosta@DESKTOP-H317250.mshome.net>
2019-08-24 14:12:09 +0900

    리펙토링 6번째 : 더 효율적인 알고리즘으로 변경한다.

--- src/code/refactoring/methodarrange_6/Account.java
+++ src/code/refactoring/methodarrange_6/Account.java
@@ -5,14 +5,17 @@ public class Account {
 	public Account() {
 
 	}
-	
+	/*
+	//as-is
 	public int gamma (int inputVal, int quantity, int yearToDate) {
 		
 		int importantValue1 = (inputVal * quantity) + delta();
 		int importantValue2 = (inputVal * yearToDate) + 100;
 		
-		if ((yearToDate - importantValue1) > 100)
+		if ((yearToDate - importantValue1) > 100) {
 			importantValue2 -= 20;
+		}
+
 		int importantValue3 = importantValue2 * 7;
 		
 		//  중략...		
@@ -20,10 +23,29 @@ public class Account {
 		return importantValue3 -2 * importantValue1;
 		
 	}
-
+	
+	//필요없는 메서드는 삭제
 	private int delta() {
 		// TODO Auto-generated method stub
 		return 0;
+	}
+	*/
+	
+	//to-be
+	public int gamma (int inputVal, int quantity, int yearToDate) {
+		
+		int importantValue1 = inputVal * quantity;
+		int importantValue2 = (inputVal * yearToDate) + 100;
+		
+		if ((yearToDate - importantValue1) > 100) {
+			importantValue2 -= 20;
+		}
+
+		int importantValue3 = importantValue2 * 7;
+		
+
+		return importantValue3 -2 * importantValue1;
+		
 	}
 
 }
