39eee61a02ec7ec600e1cbf055d4552d82319b7d
kosta <kosta@DESKTOP-H317250.mshome.net>
2019-08-24 11:09:02 +0900

    테스트 실습

---
+++ src/code/refactoring/junit/ArrayListTest2.java
@@ -0,0 +1,36 @@
+package code.refactoring.junit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ArrayListTest2 {
+
+	
+	/*
+	 * java > junit case > 테스트 대상 클래스 지정  > 테스트 대상 메서드 지정
+	 * 
+	 */
+	
+	
+	@Test
+	public void testAddE() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddAllCollectionOfQextendsE() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSet() {
+		fail("Not yet implemented");
+	}
+
+}
