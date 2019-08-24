70494cdb261930f83f31bde88de856938ccdb36f
kosta <kosta@DESKTOP-H317250.mshome.net>
2019-08-24 16:24:41 +0900

    메서드 호출 단순화 1 : 밖에서 봤을때는 하나인것 같지만 안에서는 다른 로직이 곂침 두개로 나눠야함

--- src/code/refactoring/methodsimple_1/SecurityMonitor.java
+++ src/code/refactoring/methodsimple_1/SecurityMonitor.java
@@ -1,13 +1,37 @@
 package code.refactoring.methodsimple_1;
 
 public class SecurityMonitor {
-	
-	// 침입자 이름을 알려주고 경고메시지를 보냄
+
+  // 침입자 이름을 알려주고 경고메시지를 보냄
   public void checkSecurity(String[] people) {
-	  String[] found = foundMiscreant(people);
+	  //String[] found = foundMiscreant(people);
+	  sendAlert(people);
+	  String[] found = foundPerson(people);
 	  someLaterCode(found);
   }
+  
+  // to-be : 밖에서 봤을때는 하나인것 같지만 안에서는 다른 로직이 곂침 두개로 나눠야함
+  private void sendAlert(String[] people) {
+	  for (int i = 0; i < people.length; i++) {
+		  sendAlertMsg();
+	  }
+  }
+  // to-be : 밖에서 봤을때는 하나인것 같지만 안에서는 다른 로직이 곂침 두개로 나눠야함
+  private String[] foundPerson(String[] people) {
+	  String[] found = new String[people.length];
 	  
+	  for (int i = 0; i < people.length; i++) {
+		  if (people[i].equals ("Don")){
+			  found[i] = "Don";
+		  }
+		  if (people[i].equals ("John")){
+			  found[i] = "John";
+		  }
+	  }
+	  return found;
+  }
+
+  //as-is
   private String[] foundMiscreant(String[] people){
 	  
 	  String[] found = new String[people.length];
@@ -24,6 +48,7 @@ public class SecurityMonitor {
 	  }
 	  return found;
   }
+  
   private void sendAlertMsg(){
 	  System.out.println( "Alert" );	 
   }
