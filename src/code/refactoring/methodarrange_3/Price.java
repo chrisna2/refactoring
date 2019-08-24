60a8003cce4fa2d491bad2e263b34314b5033e2d
kosta <kosta@DESKTOP-H317250.mshome.net>
2019-08-24 16:01:18 +0900

    메서드 3번째 : 직관적 임시 변수 사용, 사용된 수식이 복잡할 때 수식의 결과 -> 일부를 임시변수로 처리한다.

--- src/code/refactoring/methodarrange_3/Price.java
+++ src/code/refactoring/methodarrange_3/Price.java
@@ -10,15 +10,6 @@ public class Price {
 		this._itemPrice = itemPrice;
 	}
 
-	/*
-	// as-is
-	public double price() {
-		//결제액(price) = 총구매액(base price) - 대량구매할인(discount) + 배송비(shipping)
-		return _quantity * _itemPrice - 
-				Math.max(0,  _quantity - 500) * _itemPrice * 0.05 + 
-				Math.min(_quantity * _itemPrice * 0.1,  1000.0);
-	}
-	*/
 	
 	// to-be
 	// 매서드 정리 : 직관적 임시 변수 사용 : 사용된 수식이 복잡할 때 수식의 결과, 일부를 임시변수로 처리한다.
@@ -33,6 +24,14 @@ public class Price {
 		return basePrice - discount + shipping;
 	}
 	
-	
+	/*
+	// as-is
+	public double price() {
+		//결제액(price) = 총구매액(base price) - 대량구매할인(discount) + 배송비(shipping)
+		return _quantity * _itemPrice - 
+				Math.max(0,  _quantity - 500) * _itemPrice * 0.05 + 
+				Math.min(_quantity * _itemPrice * 0.1,  1000.0);
+	}
+	 */
 	
 }
