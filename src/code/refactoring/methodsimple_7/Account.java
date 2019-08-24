2b60eb05efc6a398c7d62871b42f0e046b4cdb3b
kosta <kosta@DESKTOP-H317250.mshome.net>
2019-08-24 17:36:44 +0900

    메서드호출 단순화 7 : 항상 예외처리는 try ~ catch ~ 로 합니다.

--- src/code/refactoring/methodsimple_7/Account.java
+++ src/code/refactoring/methodsimple_7/Account.java
@@ -3,6 +3,16 @@ package code.refactoring.methodsimple_7;
 public class Account {
 	private int _balance=100;
 	
+	//TO-BE
+	void canWithdraw(int amount) {
+		if (amount > _balance) {
+			throw new IllegalArgumentException("입력된 값이 너무 크다!");
+		}
+		_balance -= amount;
+	}
+	
+	/*
+	//AS-IS
 	int canWithdraw(int amount) {
 		if (amount > _balance)
         	return -1;
@@ -10,6 +20,6 @@ public class Account {
         	_balance -= amount;
         	return 0;
     	}
-	}
+	}*/
 
 }
--- src/code/refactoring/methodsimple_7/AccountTest.java
+++ src/code/refactoring/methodsimple_7/AccountTest.java
@@ -3,6 +3,23 @@ package code.refactoring.methodsimple_7;
 public class AccountTest {
 	
 	public static void main(String[] args) { 
+		Account account= new Account();
+		int amount = 150;
+		try {
+			account.canWithdraw(amount);
+			doTheUsualThing();
+		}
+		catch (Exception e) {
+			System.out.println(e.getMessage());
+			handleOverdrawn();
+			// <!> try ~ catch 이후 로직을 추가하게 되면 애러나도 그대로 실행된다.
+		}
+	}
+	
+	
+	/*
+	//as-is
+	public static void main(String[] args) { 
 	 
 		Account account= new Account();
 	
@@ -13,7 +30,7 @@ public class AccountTest {
 			account.canWithdraw(amount);
 			doTheUsualThing();
 		}
-	}
+	}*/
 
 	private static void doTheUsualThing() {
 		System.out.println( "doTheUsualThing() -->" ); 
