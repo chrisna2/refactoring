7fdce1fd48d654e181e6caea6bb082f2f3c9332b
kosta <kosta@DESKTOP-H317250.mshome.net>
2019-08-24 17:43:37 +0900

    메서드호출 단순화 8 : 그렇다고 필요없는 곳에 항상 예외처리는 try ~ catch ~ 로 하지는 말것

--- src/code/refactoring/methodsimple_8/ResourcePool.java
+++ src/code/refactoring/methodsimple_8/ResourcePool.java
@@ -12,7 +12,26 @@ public class ResourcePool {
 		  _available=new Stack();
 		  _allocated=new Stack();
 	}
-	
+
+	//to-be : 
+	Resource getResource() {
+		
+	    Resource result;
+	    
+	    if(_available.isEmpty()) {
+			result = new Resource();
+			_allocated.push(result);
+			return result;
+	    }
+	    
+	 	result = (Resource)_available.pop();
+	 	_allocated.push(result);
+	 	return result;
+	    // Resource 고갈을 예기치못한 일이 아니므로 예외처리를 사용하지 않는 것이 좋음
+	}
+	   
+	/*
+	//as-is : 
    Resource getResource() {
        Resource result;
        
@@ -27,5 +46,5 @@ public class ResourcePool {
        }
       
        // Resource 고갈을 예기치못한 일이 아니므로 예외처리를 사용하지 않는 것이 좋음
-   }
+   }*/
 }
