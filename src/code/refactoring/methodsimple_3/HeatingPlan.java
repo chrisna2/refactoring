6bd7957e84c25e13ecc0990e15d5a31e17c6b8ec
kosta <kosta@DESKTOP-H317250.mshome.net>
2019-08-24 16:43:19 +0900

    메서드호출 단순화 3 : 가급적 파라미터는 여러개 보다 한개의 객체형으로 보내라

--- src/code/refactoring/methodsimple_3/HeatingPlan.java
+++ src/code/refactoring/methodsimple_3/HeatingPlan.java
@@ -2,8 +2,16 @@ package code.refactoring.methodsimple_3;
 
 public class HeatingPlan {
 	private TempRange _range = new TempRange();
-		
+	
+	//to-be
+	boolean withinRange (TempRange roomRage) {
+	    return (roomRage.getLow() >= _range.getLow() && roomRage.getHigh() <= _range.getHigh());
+	}
+	
+	/*
+	//as-is
 	boolean withinRange (int low, int high) {
 	    return (low >= _range.getLow() && high <= _range.getHigh());
 	}
+	*/
 }
--- src/code/refactoring/methodsimple_3/Room.java
+++ src/code/refactoring/methodsimple_3/Room.java
@@ -2,9 +2,13 @@ package code.refactoring.methodsimple_3;
 
 public class Room {
 	boolean withinPlan(HeatingPlan plan) {
-	    int low = daysTempRange().getLow();
-	    int high = daysTempRange().getHigh();
-	    return plan.withinRange(low, high);
+	   
+		//int low = daysTempRange().getLow();
+	    //int high = daysTempRange().getHigh();
+	    
+	    return plan.withinRange(daysTempRange());
+
+	    // return plan.withinRange(low, high);
 	}
 	
 	TempRange daysTempRange() {   
