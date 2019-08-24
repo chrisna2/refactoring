bcf11dd0dc619564a9bc1c7c955159475144b8cf
kosta <kosta@DESKTOP-H317250.mshome.net>
2019-08-24 13:55:17 +0900

    리펙토링 5번째 : 매개변수를 그대로 리턴시키지 마라 지역변수로 사용해라

--- src/code/refactoring/methodarrange_5/Price.java
+++ src/code/refactoring/methodarrange_5/Price.java
@@ -2,14 +2,29 @@ package code.refactoring.methodarrange_5;
 
 public class Price {
 	
+	/*
+	//as-is
 	public int discount (int inputVal, int quantity, int yearToDate) {
-
+		
 	    if (inputVal > 50)  inputVal -= 2;
 	    if (quantity > 100) inputVal -= 1;
 	    if (yearToDate > 10000) inputVal -= 4;
 
 	    return inputVal;
 	}
+	*/
+	
+	//to-be
+	public int discount (int inputVal, int quantity, int yearToDate) {
+		
+		int result = inputVal;
+		
+	    if (inputVal > 50)  result -= 2;
+	    if (quantity > 100) result -= 1;
+	    if (yearToDate > 10000) result -= 4;
 
+	    return result;
+	}
+	
 
 }
