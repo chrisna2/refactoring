47f9a04dc9354a5ae4db2038c7b99fac87e793ff
kosta <kosta@DESKTOP-H317250.mshome.net>
2019-08-24 15:48:05 +0900

    조건문 6: Assertion 넣기 -> 읽고 집에서 해봄

--- src/code/refactoring/conditionsimple_6/Employee.java
+++ src/code/refactoring/conditionsimple_6/Employee.java
@@ -1,6 +1,7 @@
 package code.refactoring.conditionsimple_6;
 
 public class Employee {
+	//as-is
 	private static final double NULL_EXPENSE = -1.0;
 	private double _expenseLimit = NULL_EXPENSE;
 	private Project _primaryProject;
