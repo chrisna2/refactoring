f3c945f0c56902738a4a16917a18bbf6c256b823
kosta <kosta@DESKTOP-H317250.mshome.net>
2019-08-31 14:44:57 +0900

    일반화 처리 1 : 생성자 내용 상향

--- src/code/refactoring/general_1/Employee.java
+++ src/code/refactoring/general_1/Employee.java
@@ -5,6 +5,18 @@ public class Employee {
 	protected String _name;
 	protected String _id;
 	
+	public Employee(String _name, String _id) {
+		this._name = _name;
+		this._id = _id;
+	}
+	
+	//하위 클래스의 공통된 메소드 : protected 상속된 클래스만 접근이 가능, 외부에서는 private와 마찬가지로 접근 불가능
+	protected void initialize() {
+		if (isPriviliged()) {
+			assignCar();   //  모든 하위클래스의 공통기능
+		}
+	}
+	
 	boolean isPriviliged() {
 		return false;
 		
--- src/code/refactoring/general_1/Manager.java
+++ src/code/refactoring/general_1/Manager.java
@@ -1,9 +1,21 @@
 package code.refactoring.general_1;
 
+/*
+ * 일반화 처리 1 : 생성자 내용 상향
+ * 
+ */
 public class Manager extends Employee {
 	
 	private int _grade;
 	
+	//to-be
+	public Manager (String name, String id, int grade) {
+		super(name, id);
+		_grade = grade;
+		initialize();
+	}
+	/*
+	//as-is
 	public Manager (String name, String id, int grade) {
 		_name = name;
 		_id = id;
@@ -11,9 +23,9 @@ public class Manager extends Employee {
 		
 		if (isPriviliged()) assignCar();   //  모든 하위클래스의 공통기능
 	}
-	
+	*/
 	boolean isPriviliged() {
 		return _grade > 4;
 	}
-
 }
+	
