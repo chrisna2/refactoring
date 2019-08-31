a03d493dd92028224300e517bb2a650d9e64d6f9
kosta <kosta@DESKTOP-H317250.mshome.net>
2019-08-31 09:33:00 +0900

    객체 구조화 1 : 메서드 이동

--- src/code/refactoring/classmove_1/Account.java
+++ src/code/refactoring/classmove_1/Account.java
@@ -1,7 +1,38 @@
 package code.refactoring.classmove_1;
 
+/**
+ * 객체 구조화
+ * 메서드 이동 : 메서드가 다른 클래스의 기능 변수를 더 사용할 때 다른 클래스로 이동한다.
+ * 
+ * @author chrisna2
+ *
+ */
 public class Account {
+	
+	//to-be
+	private AccountType _type = new AccountType();
+	private int _daysOverdrawn;
+	
+	public Account(int daysOverdrawn) {
+		this._daysOverdrawn = daysOverdrawn;
 
+	}
+	
+	//테스트 코드(외부)에서 사용이 되므로 : 델리게이션 처리를 해준다
+	public double overdraftCharge() {
+		// 이렇게 한줄만 써있고 다른 메서드를 호출 하는 코드들이 있는데 용어로 deligation 처리라고 한다.
+		return _type.overdraftCharge(_daysOverdrawn);
+	}
+	
+	double bankCharge() {
+		double result = 4.5;
+		if(_daysOverdrawn > 0) {
+			result += _type.overdraftCharge(_daysOverdrawn);
+		}
+		return result;	
+	}
+	
+	/* AS-IS
 	private AccountType _type = new AccountType();
 	private int _daysOverdrawn;
 	
@@ -25,4 +56,5 @@ public class Account {
 		if(_daysOverdrawn > 0) result += overdraftCharge();
 		return result;	
 	}
+	*/
 }
--- src/code/refactoring/classmove_1/AccountType.java
+++ src/code/refactoring/classmove_1/AccountType.java
@@ -6,5 +6,15 @@ public class AccountType {
 		// TODO Auto-generated method stub
 		return true;
 	}
+	
+	public double overdraftCharge(int daysOverdrawn) {
+		
+		if (isPremium()) {
+			double result = 10;
+			if(daysOverdrawn > 7) result += (daysOverdrawn - 7) *0.85;
+				return result;			
+		}
+		else return daysOverdrawn * 1.75;
+	}
 
 }
