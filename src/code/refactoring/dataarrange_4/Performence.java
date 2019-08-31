717caf0675a8e41e66efe085119533f9bc435876
kosta <kosta@DESKTOP-H317250.mshome.net>
2019-08-31 13:38:28 +0900

    데이터 구조화 3 : 배열형태의 데이터는  데이터 객체로 전환

---
+++ src/code/refactoring/dataarrange_4/Performence.java
@@ -0,0 +1,20 @@
+package code.refactoring.dataarrange_4;
+
+public class Performence {
+	
+	private String teamName;
+	private int winsGame;
+	
+	public String getTeamName() {
+		return teamName;
+	}
+	public void setTeamName(String teamName) {
+		this.teamName = teamName;
+	}
+	public int getWinsGame() {
+		return winsGame;
+	}
+	public void setWinsGame(int winsGame) {
+		this.winsGame = winsGame;
+	}
+}
--- src/code/refactoring/dataarrange_4/SoccerTeam.java
+++ src/code/refactoring/dataarrange_4/SoccerTeam.java
@@ -1,8 +1,27 @@
 package code.refactoring.dataarrange_4;
 
+/**
+ * 데이터 구조화 3 : 배열형태의 데이터는  데이터 객체로 전환
+ * @author kosta
+ *
+ */
 public class SoccerTeam {
 
 	 public static void main(String[] args){ 
+		 
+		 //to-be
+		 Performence gamePerformence = new Performence();
+		 
+		 gamePerformence.setTeamName("리버풀");
+		 gamePerformence.setWinsGame(15);
+		 
+		 String name = gamePerformence.getTeamName();
+		 int wins = gamePerformence.getWinsGame();
+		 
+		 System.out.println("name --> " + name);
+		 System.out.println("wins --> " + wins);
+		 
+		 /*as-is
 		 String[] row = new String[3];
 		 
 		 row[0] = "리버풀";
@@ -13,6 +32,6 @@ public class SoccerTeam {
 		 
 		 System.out.println("name --> " + name);
 		 System.out.println("wins --> " + wins);
-
+		 */
 	 }
 }
