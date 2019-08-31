9770dc75f06da3f8e928407ceacc26bc404fc791
kosta <kosta@DESKTOP-H317250.mshome.net>
2019-08-31 14:12:35 +0900

    데이터 구조화 4 : 컬랙션 캡슐화

--- src/code/refactoring/dataarrange_6/Person.java
+++ src/code/refactoring/dataarrange_6/Person.java
@@ -1,18 +1,48 @@
 package code.refactoring.dataarrange_6;
 
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
+//데이터 클래스에서 객체 그대로를 호출하여 클라이언트 상에서 추가로 처리하는 것은 별로다.
 public class Person {
 	
 	private Set _courses;
 	
-	public Set getCourses() {
-		return _courses;
+	//as-is
+//	public Set getCourses() {
+//		return _courses;
+//	}
+//	public void setCourses(Set arg) {
+//		_courses = arg;
+//	}
+	
+	public Person() {
+		_courses = new HashSet();
 	}
-	public void setCourses(Set arg) {
-		_courses = arg;
+	
+	//to-be
+	public void addCource(Course arg) {
+		_courses.add(arg);
 	}
-
+	
+	public void removeCource(Course arg) {
+		_courses.remove(arg);
+	}
+	public int getSize() {
+		return _courses.size();
+	}
+	
+	public int numberOfAdvancedCourses() {
+		Iterator iter = _courses.iterator();
+		
+		int count = 0;
+		while (iter.hasNext()) {
+			Course each = (Course) iter.next();
+			if (each.isAdvanced()) count ++;
+		}
+		return count;
+	}
 
 }
 
--- src/code/refactoring/dataarrange_6/PersonTest.java
+++ src/code/refactoring/dataarrange_6/PersonTest.java
@@ -19,27 +19,22 @@ public class PersonTest {
 	public void testPerson() {
 		// 과정을 추가하는 코드
 		Person kent = new Person();
-		Set s = new HashSet();
-		s.add(new Course ("Smalltalk Programming", false));
-		s.add(new Course ("Appreciating Single Malts", true));
-		kent.setCourses(s);
-        assertEquals(2, kent.getCourses().size());
+		
+		kent.addCource(new Course ("Smalltalk Programming", false));
+		kent.addCource(new Course ("Appreciating Single Malts", true));
+		
+        assertEquals(2, kent.getSize());
 
 		Course refact = new Course ("Refactoring", true);
-		kent.getCourses().add(refact);
-		kent.getCourses().add(new Course ("Brutal Sarcasm", false));
-		assertEquals(4, kent.getCourses().size());
+		kent.addCource(refact);
+		kent.addCource(new Course ("Brutal Sarcasm", false));
+		assertEquals(4, kent.getSize());
 
-		kent.getCourses().remove(refact);
-		assertEquals(3, kent.getCourses().size());
+		kent.removeCource(refact);
+		assertEquals(3, kent.getSize());
 	  
 		// 고급과정 검색
-		Iterator iter = kent.getCourses().iterator();
-		int count = 0;
-		while (iter.hasNext()) {
-			Course each = (Course) iter.next();
-			if (each.isAdvanced()) count ++;
-		}
+		int count = kent.numberOfAdvancedCourses();
 		System.out.println( count );
 	}
 
