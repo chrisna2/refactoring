df9ca27dd63a331ab3bcab0a7fdfa04c8b303231
kosta <kosta@DESKTOP-H317250.mshome.net>
2019-08-31 09:52:58 +0900

    객체구조화 3 : 클래스 추출

--- src/code/refactoring/classmove_3/Person.java
+++ src/code/refactoring/classmove_3/Person.java
@@ -1,7 +1,39 @@
 package code.refactoring.classmove_3;
 
+/**
+ * 객체구조화 3 : 클래스 추출
+ * @author kosta
+ *
+ */
 public class Person {
 	
+	private String name;
+	
+	private TelephoneNumber telNumber = new TelephoneNumber();
+	
+	public String getOfficeAreaCode() {
+		return telNumber.getOfficeAreaCode();
+	}
+	public void setOfficeAreaCode(String officeAreaCode) {
+		telNumber.setOfficeAreaCode(officeAreaCode);
+	}
+	public String getOfficeNumber() {
+		return telNumber.getOfficeNumber();
+	}
+	public void setOfficeNumber(String officeNumber) {
+		telNumber.setOfficeNumber(officeNumber);
+	}
+	public String getTelephoneNumber() {
+		return telNumber.getTelephoneNumber();
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	/*AS-IS
 	private String name;
 	private String officeAreaCode;
 	private String officeNumber;
@@ -27,5 +59,5 @@ public class Person {
 	
 	public String getTelephoneNumber() {
 		return ("(" + officeAreaCode + ")" + officeNumber);
-	}
+	}*/
 }
---
+++ src/code/refactoring/classmove_3/TelephoneNumber.java
@@ -0,0 +1,23 @@
+package code.refactoring.classmove_3;
+
+public class TelephoneNumber {
+	
+	private String officeAreaCode;
+	private String officeNumber;
+	
+	public String getOfficeAreaCode() {
+		return officeAreaCode;
+	}
+	public void setOfficeAreaCode(String officeAreaCode) {
+		this.officeAreaCode = officeAreaCode;
+	}
+	public String getOfficeNumber() {
+		return officeNumber;
+	}
+	public void setOfficeNumber(String officeNumber) {
+		this.officeNumber = officeNumber;
+	}
+	public String getTelephoneNumber() {
+		return ("(" + officeAreaCode + ")" + officeNumber);
+	}
+}
